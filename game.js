1d5ae8aebad884ba0394a229bbb0cf9552f5719a
DonChatelain <djchatelain@gmail.com>
2015-09-30 13:06:17 -0700

    covered possible user inputs being different letter cases as well as a 'Y'

--- game.js
+++ game.js
@@ -2,18 +2,18 @@ var numCorrect = 0;
 
 var q1 = prompt("Do I like pizza? (yes or no)");
 console.log(q1);
-if(q1 === "yes") {
+if(q1.toUpperCase() === "YES" || q1.toUpperCase() === "Y") {
 	numCorrect++;
 }
 var q2 = prompt("Do I like beer? (yes or no)");
 console.log(q2);
-if(q2 === "yes"){
+if(q2.toUpperCase() === "YES" || q2.toUpperCase() === "Y"){
 	numCorrect++;
 }
 
 var q3 = prompt("Did it occur to me just now that I should leave what I'm doing and go get pizza and beer? (yes or no)");
 console.log(q3);
-if(q3 === "yes"){
+if(q3.toUpperCase() === "YES" || q3.toUpperCase() === "Y"){
 	numCorrect++;
 }
 
