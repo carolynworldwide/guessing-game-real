2ee412b821aa357383bb83e3977e823d661da374
DonChatelain <djchatelain@gmail.com>
2015-10-01 11:51:14 -0700

    set up skeleton for functions

--- game.js
+++ game.js
@@ -1,7 +1,9 @@
 var numCorrect = 0;
 
 var q1 = prompt("Do I like pizza? (yes or no)");
+
 console.log(q1);
+
 if(q1.toUpperCase() === "YES" || q1.toUpperCase() === "Y") {
 	numCorrect++;
 	alert('Correct! Of course I love pizza!');
@@ -9,7 +11,9 @@ if(q1.toUpperCase() === "YES" || q1.toUpperCase() === "Y") {
 	alert('Wrong, I actually like pizza of all sorts.');
 }
 var q2 = prompt("Do I like beer? (yes or no)");
+
 console.log(q2);
+
 if(q2.toUpperCase() === "YES" || q2.toUpperCase() === "Y"){
 	numCorrect++;
 	alert('Correct! I\'m a beeraholic!');
@@ -18,7 +22,9 @@ if(q2.toUpperCase() === "YES" || q2.toUpperCase() === "Y"){
 }
 
 var q3 = prompt("Did it occur to me just now that I should leave what I'm doing and go get pizza and beer? (yes or no)");
+
 console.log(q3);
+
 if(q3.toUpperCase() === "YES" || q3.toUpperCase() === "Y"){
 	numCorrect++;
 	alert('Correct! Let\'s go!');
@@ -28,7 +34,15 @@ if(q3.toUpperCase() === "YES" || q3.toUpperCase() === "Y"){
 
 alert("You got " + numCorrect + " out of 3 questions right. Thanks for playing!");
 
-//alert("Your answer to question 1 is: " + q1);
-//alert("Your answer to question 2 is: " + q2);
-//alert("Your answer to question 3 is: " + q3);
 
+function q1() {
+
+}
+
+function q2() {
+	
+}
+
+function q3() {
+	
+}
