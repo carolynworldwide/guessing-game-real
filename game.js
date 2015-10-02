3db69ff840f6a46c74aec0c7dd2b305313a056a3
carolynworldwide <carolynworldwide@gmail.com>
2015-10-02 14:47:20 -0700

    add images to correct answers

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 var numCorrect = 0;
-
 var elOne = document.getElementById("one");
 var elTwo = document.getElementById("two");
 var elThree = document.getElementById("three");
 var elResults = document.getElementById("results");
 var questions = ["Do I like pizza?", "Do I like beer?", "Did it occur to me just now that I should leave what I'm doing and go get pizza and beer?"];
 var answers = ["YES", "Y", "NO", "N"];	
+var elImg1 = document.getElementById('img_contain1');
+var elImg2 = document.getElementById('img_contain2');
+var elImg3 = document.getElementById('img_contain3');
 
 function question1() {
 	var q1 = prompt("Do I like pizza?");
@@ -14,6 +16,7 @@ function question1() {
 
 	if(q1.toUpperCase() === "YES" || q1.toUpperCase() === "Y") {
 		numCorrect++;
+		elImg1.innerHTML = ("<img src='images/success-kid.png' />");
 		elOne.textContent=('Correct! Of course I love pizza!');
 	} else {
 		elOne.textContent=('Wrong, I actually like pizza of all sorts.');
@@ -27,6 +30,7 @@ function question2() {
 
 	if(q2.toUpperCase() === "YES" || q2.toUpperCase() === "Y"){
 		numCorrect++;
+		elImg2.innerHTML = ("<img src='images/success-rice.jpg' />");
 		elTwo.textContent=('Correct! I\'m a beeraholic!');
 	} else {
 		elTwo.textContent=('Wrong, who doesn\'t love a good brew?');
@@ -41,15 +45,21 @@ function question3() {
 
 	if(q3.toUpperCase() === "YES" || q3.toUpperCase() === "Y"){
 		numCorrect++;
+		elImg3.innerHTML = ("<img src='images/success-Colbert.gif-c200' />");
 		elThree.textContent=('Correct! Let\'s go!');
 	} else {
 		elThree.textContent=('Wrong, just go home...');
 	}	
 }
 
+function getResults(){
+		elResults.textContent=("You got " + numCorrect + " out of 3 questions right. Thanks for playing!");
+}
+
 question1();
-question2();
-question3();
+window.setTimeout(question2, 500);
+window.setTimeout(question3, 1000);
+window.setTimeout(getResults, 1500);
 
 
-elResults.textContent=("You got " + numCorrect + " out of 3 questions right. Thanks for playing!");
+console.log(numCorrect);
