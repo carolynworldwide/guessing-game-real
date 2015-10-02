a07fdb5f60339730bda2a810658e75bbba6fceda
carolynworldwide <carolynworldwide@gmail.com>
2015-10-02 15:23:59 -0700

    update code style

--- game.js
+++ game.js
@@ -12,60 +12,50 @@ var elImg3 = document.getElementById('img_contain3');
 function question1() {
 	var q1 = prompt("Do I like pizza?");
 
-	console.log(q1);
-
-	if(q1.toUpperCase() === "YES" || q1.toUpperCase() === "Y") {
+	if (q1.toUpperCase() === "YES" || q1.toUpperCase() === "Y") {
 		numCorrect++;
 		elImg1.innerHTML = ("<img src='images/success-kid.png' />");
 		elOne.className = ('correct');
 		elOne.textContent = ('Correct! Of course I love pizza!');
 	} else {
 		elOne.className = ('incorrect');
-		elOne.textContent=('Wrong, I actually like pizza of all sorts.');
+		elOne.textContent = ('Wrong, I actually like pizza of all sorts.');
 	}
 }
 
 function question2() {
 	var q2 = prompt("Do I like beer?");
 
-	console.log(q2);
-
-	if(q2.toUpperCase() === "YES" || q2.toUpperCase() === "Y"){
+	if (q2.toUpperCase() === "YES" || q2.toUpperCase() === "Y"){
 		numCorrect++;
 		elImg2.innerHTML = ("<img src='images/success-rice.jpg' />");
 		elTwo.className = ('correct');
-		elTwo.textContent=('Correct! I\'m a beeraholic!');
+		elTwo.textContent = ('Correct! I\'m a beeraholic!');
 	} else {
 		elTwo.className = ('incorrect');
-		elTwo.textContent=('Wrong, who doesn\'t love a good brew?');
+		elTwo.textContent = ('Wrong, who doesn\'t love a good brew?');
 	}
-
 }
 
 function question3() {
 	var q3 = prompt("Did it occur to me just now that I should leave what I'm doing and go get pizza and beer?");
 
-	console.log(q3);
-
-	if(q3.toUpperCase() === "YES" || q3.toUpperCase() === "Y"){
+	if (q3.toUpperCase() === "YES" || q3.toUpperCase() === "Y"){
 		numCorrect++;
 		elImg3.innerHTML = ("<img src='images/success-Colbert.gif-c200' />");
 		elThree.className = ('correct');
-		elThree.textContent=('Correct! Let\'s go!');
+		elThree.textContent = ('Correct! Let\'s go!');
 	} else {
 		elThree.className = ('incorrect');
-		elThree.textContent=('Wrong, just go home...');
+		elThree.textContent = ('Wrong, just go home...');
 	}	
 }
 
 function getResults(){
-		elResults.textContent=("You got " + numCorrect + " out of 3 questions right. Thanks for playing!");
+		elResults.textContent = ("You got " + numCorrect + " out of 3 questions right. Thanks for playing!");
 }
 
 question1();
 window.setTimeout(question2, 500);
 window.setTimeout(question3, 1000);
 window.setTimeout(getResults, 1500);
-
-
-console.log(numCorrect);
